e2f926ec9407e4b5fd76bcbd1945e17acb37af58
Arnaud Rocher <arnaud.roche3@gmail.com>
2022-05-12 20:24:18 +0000

    style: sort imports

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,10 @@
-/* eslint-env node */
+const tsConfig = require("./tsconfig.json")
+const internalPathPatterns = Object.keys(tsConfig.compilerOptions.paths)
+  .filter((pat) => pat.endsWith("/*"))
+  .map((pat) => pat.slice(0, -2))
+  .join(",")
+const internalPathGlob = `{${internalPathPatterns}}/**`
+
 require("@rushstack/eslint-patch/modern-module-resolution")
 
 module.exports = {
@@ -11,8 +17,44 @@ module.exports = {
     "@vue/eslint-config-prettier",
   ],
 
+  plugins: ["import"],
+
   rules: {
     quotes: ["warn", "double", { avoidEscape: true }],
+
+    // enforce usage of type-only imports (recommended with Vite)
+    // https://vitejs.dev/guide/features.html#typescript
+    "@typescript-eslint/consistent-type-imports": "error",
+
+    // allow debugger during development only
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+
+    "import/order": [
+      "error",
+      {
+        "newlines-between": "always",
+        alphabetize: { order: "asc" },
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+          "object",
+          "type",
+        ],
+        pathGroups: [
+          {
+            pattern: internalPathGlob,
+            group: "internal",
+          },
+        ],
+        pathGroupsExcludedImportTypes: ["builtin", "type"],
+      },
+    ],
+
+    "sort-imports": ["error", { ignoreDeclarationSort: true }],
   },
 
   env: {
